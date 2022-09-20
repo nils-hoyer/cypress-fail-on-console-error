8d98b019afdbc2405be5433f06f51432b4c85344
nils-hoyer <hoyer.nils@gmail.com>
2022-09-20 22:32:06 +0200

    improve cypress command tests

--- cypress/e2e/shouldFailOnConsoleErrorFromSetConfig.cy.ts
+++ cypress/e2e/shouldFailOnConsoleErrorFromSetConfig.cy.ts
@@ -1,19 +1,35 @@
 describe('shouldFailOnConsoleErrorFromSetConfig', () => {
-    it('should throw AssertionError on console.error with setConsoleMessages', () => {
-        cy.setConsoleMessages([]).then(() => {
-            cy.visit('./cypress/fixtures/consoleErrorNotExcludedMessage.html');
+    it('should pass with getConsoleMessages', () => {
+        cy.getConsoleMessages().then((consoleMessages) => {
+            expect(consoleMessages).deep.equal([
+                /firstErrorExcluded.*/,
+                'secondErrorExcluded',
+                'thirdErrorExcluded.*consoleError.*',
+            ]);
         });
+    });
+    it('should throw AssertionError on console.error with setConsoleMessages', () => {
+        cy.setConsoleMessages([]);
+        cy.getConsoleMessages().then((consoleMessages) => {
+            expect(consoleMessages).deep.equal([]);
+        });
+        cy.visit('./cypress/fixtures/consoleErrorNotExcludedMessage.html');
     });
 
     it('should pass AssertionError on console.error with addConsoleMessages', () => {
-        cy.addConsoleMessages(['errorNotExcluded']).then(() => {
-            cy.visit('./cypress/fixtures/consoleErrorNotExcludedMessage.html');
+        cy.addConsoleMessages(['errorNotExcluded']);
+        cy.getConsoleMessages().then((consoleMessages) => {
+            expect(consoleMessages).includes('errorNotExcluded');
         });
+        cy.visit('./cypress/fixtures/consoleErrorNotExcludedMessage.html');
     });
 
     it('should throw AssertionError on console.error with deleteConsoleMessages', () => {
-        cy.deleteConsoleMessages(['secondErrorExcluded']).then(() => {
-            cy.visit('./cypress/fixtures/consoleErrorExcludeMessage.html');
+        cy.deleteConsoleMessages(['secondErrorExcluded']);
+        cy.getConsoleMessages().then((consoleMessages) => {
+            expect(consoleMessages).not.includes('errorNotExcluded');
+            expect(consoleMessages).to.have.length(3);
         });
+        cy.visit('./cypress/fixtures/consoleErrorExcludeMessage.html');
     });
 });
