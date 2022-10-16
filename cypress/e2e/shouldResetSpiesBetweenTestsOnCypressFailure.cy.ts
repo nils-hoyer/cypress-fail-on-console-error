190b2d91b7b41774b10b9d69cdc08d52d438bec7
nils-hoyer <hoyer.nils@gmail.com>
2022-10-16 23:53:08 +0200

    update fixture file name

--- cypress/e2e/shouldResetSpiesBetweenTestsOnCypressFailure.cy.ts
+++ cypress/e2e/shouldResetSpiesBetweenTestsOnCypressFailure.cy.ts
@@ -1,6 +1,6 @@
 describe('should reset spies between tests on cypress failure', () => {
     it('should throw cypress error', () => {
-        cy.visit('./cypress/fixtures/fail.html');
+        cy.visit('./cypress/fixtures/consoleErrorWithTimeout.html');
         cy.get('.notExisting', { timeout: 2000 }).should('be.visible');
     });
 
